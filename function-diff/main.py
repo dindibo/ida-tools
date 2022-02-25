b495860ce63ad7bf742966a858731e186f4a8b90
Niv <dindibo4@gmail.com>
2022-02-25 13:54:19 +0200

    Added tool to differentiate 2 program disassembly functions

--- function-diff/main.py
+++ function-diff/main.py
@@ -1,5 +1,9 @@
+from ast import arg
+from hmac import digest
 import re
 import hashlib
+from sys import argv, exit
+
 
 hex_dec = '0123456789ABCDEF'
 proc_token = ' S U B R O U T I N E '
@@ -78,36 +82,88 @@ def signify_funcs(func_opcodes):
 
 
 def key_by_value(mydict, val):
-    return mydict.keys()[mydict.values().index(val)]
+    return list(mydict.keys())[list(mydict.values()).index(val)]
 
 
-def diff_progs(sig_prog1, sig_prog2):
+def diff_progs(sig_prog1, sig_prog2, op='^'):
     hashes = [list(sig_prog1.values()), list(sig_prog2.values())]
 
     a = set(hashes[0])
     b = set(hashes[1])
 
-    diff = a ^ b
+    if op == '^':
+        diff = a ^ b
+    elif op == '&':
+        diff = a & b
 
-    funcs = [[],[]]
+    funcs = None
 
-    for hash in diff:
-        if hash in a:
-            funcs[0].append(key_by_value(a, hash))
-        else:
-            funcs[1].append(key_by_value(b, hash))
+    if op  == '^':
+        funcs = [[],[]]
+
+        for hash in diff:
+            if hash in a:
+                funcs[0].append(key_by_value(sig_prog1, hash))
+            else:
+                funcs[1].append(key_by_value(sig_prog2, hash))
+
+        funcs[0].sort()
+        funcs[1].sort()
+    elif op == '&':
+        funcs = []
+
+        for hash in diff:
+            funcs.append(key_by_value(sig_prog1, hash))
 
     return funcs
 
 
-with open('data.txt', 'r', encoding="mbcs") as f:
+def convert_func_name_to_func_code(func_txts):
+    func_code = dict()
+
+    for func_name in func_txts.keys():
+        func_code[func_name] = extract_func_opcodes(func_txts[func_name])
+        func_code[func_name] = '' if func_code[func_name] == '' else gen_md5(func_code[func_name])
+
+    return func_code
+
+
+if len(argv) != 3:
+    print('Usage: {0} <Asm1> <Asm2>'.format(argv[0]))
+    exit(1)    
+
+fname1, fname2 = argv[1], argv[2]
+func_code_arr = []
+
+with open(fname1, 'r', encoding="mbcs") as f:
     dat = f.read()
+    func_txts = extract_all_funcs(dat)
+    func_code = convert_func_name_to_func_code(func_txts)
+    func_code_arr.append(func_code)
 
+with open(fname2, 'r', encoding="mbcs") as f:
+    dat = f.read()
+    func_txts = extract_all_funcs(dat)
+    func_code = convert_func_name_to_func_code(func_txts)
+    func_code_arr.append(func_code)
 
-func_txts = extract_all_funcs(dat)
-func_code = dict()
+diffs = diff_progs(func_code_arr[0], func_code_arr[1])
+sames = diff_progs(func_code_arr[0], func_code_arr[1], op='&')
 
-for func_name in func_txts.keys():
-    func_code[func_name] = extract_func_opcodes(func_txts[func_name])
+print('Different functions in {0}:'.format(fname1))
+for x in diffs[0]:
+    print(x)
 
-print(func_code['_main'])
+print('\n'*3)
+
+print('Different functions in {0}:'.format(fname2))
+
+for x in diffs[1]:
+    print(x)
+
+print('\n'*6)
+
+print('Same functions both:')
+
+for x in sames:
+    print(x)
